49c41afffbd41ce73e60566adfe95d82473be9fa
Zinnia <120154112+zinnia-frazier@users.noreply.github.com>
2024-03-03 11:53:34 -0800

    Header and footer styles

--- fulldive-vr/src/components/Footer.js
+++ fulldive-vr/src/components/Footer.js
@@ -1,9 +1,13 @@
+// React
 import React from 'react';
+
+// Styles
+import style from '../styles/styles.module.css';
 
 export default function Footer() {
     return (
-        <>
+        <footer>
             <h2>Footer</h2>
-        </>
+        </footer>
     )
 }
--- fulldive-vr/src/components/Header.js
+++ fulldive-vr/src/components/Header.js
@@ -1,12 +1,22 @@
+// React
 import React from 'react';
+
+// Styles
+import style from '../styles/styles.module.css';
 
 export default function Header(props) {
     return (
-        <>
+        
+        <header>
             <h2>Header</h2>
-            <button onClick={props.handleHome}>Home</button>
-            <button onClick={props.handleAbout}>About</button>
-            <button onClick={props.handleStore}>Store</button>
-        </>
+
+            <nav>
+                <button onClick={props.handleHome}>Home</button>
+                <button onClick={props.handleAbout}>About</button>
+                <button onClick={props.handleStore}>Store</button>
+            </nav>
+        </header>
+            
+        
     )
 }
