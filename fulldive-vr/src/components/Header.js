83bd19a54dddab5bc597c04b2d2462f9356b1f9e
Zinnia <120154112+zinnia-frazier@users.noreply.github.com>
2024-03-03 18:06:40 -0800

    Header style with grid

--- fulldive-vr/src/components/Header.js
+++ fulldive-vr/src/components/Header.js
@@ -6,22 +6,22 @@ import style from '../styles/styles.module.css';
 
 export default function Header(props) {
     return (
-        
-        <header className={style.headerContainer}>
-            <div className={style.block}>
-                <h2>Header</h2>
+        <header className={style.headerGrid}>
+            <div className={style.headText}>
+                <h2>Fulldive VR</h2>
             </div>
             
-            <div className={style.block}>
-                <nav>
-                    <button onClick={props.handleHome}>Home</button>
-                    <button onClick={props.handleAbout}>About</button>
-                    <button onClick={props.handleStore}>Store</button>
-                </nav>
+            <div className={style.headNav}>
+                <div>
+                    <nav>
+                        <button onClick={props.handleHome}>Home</button>
+                        <button onClick={props.handleAbout}>About</button>
+                        <button onClick={props.handleStore}>Store</button>
+                        <button className={style.btnPrimary}>Sign Up</button>
+                    </nav>
+                </div>
             </div>
             
         </header>
-            
-        
     )
 }
