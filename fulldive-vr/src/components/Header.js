dacb9eb94b83b53e904841d8e24981ab11b670be
Zinnia <120154112+zinnia-frazier@users.noreply.github.com>
2024-03-03 17:02:17 -0800

    Button style and header footer

--- fulldive-vr/src/components/Header.js
+++ fulldive-vr/src/components/Header.js
@@ -7,14 +7,19 @@ import style from '../styles/styles.module.css';
 export default function Header(props) {
     return (
         
-        <header>
-            <h2>Header</h2>
-
-            <nav>
-                <button onClick={props.handleHome}>Home</button>
-                <button onClick={props.handleAbout}>About</button>
-                <button onClick={props.handleStore}>Store</button>
-            </nav>
+        <header className={style.headerContainer}>
+            <div className={style.block}>
+                <h2>Header</h2>
+            </div>
+            
+            <div className={style.block}>
+                <nav>
+                    <button onClick={props.handleHome}>Home</button>
+                    <button onClick={props.handleAbout}>About</button>
+                    <button onClick={props.handleStore}>Store</button>
+                </nav>
+            </div>
+            
         </header>
             
         
