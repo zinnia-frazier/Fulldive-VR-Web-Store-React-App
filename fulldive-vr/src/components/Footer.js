adf2c367f31ab72e0204d81ba1d8987f18f0c808
Zinnia <120154112+zinnia-frazier@users.noreply.github.com>
2024-03-10 21:42:12 -0700

    Footer created

--- fulldive-vr/src/components/Footer.js
+++ fulldive-vr/src/components/Footer.js
@@ -7,7 +7,33 @@ import style from '../styles/styles.module.css';
 export default function Footer() {
     return (
         <footer>
-            <h2>Footer</h2>
+            <div className={style.container}>
+                <div className={style.block}>
+                    <h3>Company Info</h3>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Store</li>
+                    </ul>
+                </div>
+
+                <div className={style.block}>
+                    <h3>Customer Support</h3>
+                    <ul>
+                        <li>Contact</li>
+                        <li>FAQ</li>
+                    </ul>
+                </div>
+
+                <div className={style.block}>
+                    <h3>Follow Us</h3>
+                    <ul>
+                        <li>Instagram</li>
+                        <li>YouTube</li>
+                        <li>Pinterest</li>
+                    </ul>
+                </div>
+            </div>
         </footer>
     )
 }
